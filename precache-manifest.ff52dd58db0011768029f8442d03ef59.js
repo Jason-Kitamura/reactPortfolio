904bebd0002b50e3ab8d2937e7b0758dca116641
Jason-Kitamura <jasonkitamura@gmail.com>
2020-04-10 10:09:12 -0400

    Updates

---
+++ precache-manifest.ff52dd58db0011768029f8442d03ef59.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736b1554ebdc03843749ec0f887cfaac",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "2df3a75394425a5f632b",
+    "url": "/reactPortfolio/static/css/main.b6675e3c.chunk.css"
+  },
+  {
+    "revision": "bb31510c10082f83c3f8",
+    "url": "/reactPortfolio/static/js/2.116b9dec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.116b9dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df3a75394425a5f632b",
+    "url": "/reactPortfolio/static/js/main.3e00df75.chunk.js"
+  },
+  {
+    "revision": "85d7aa71eb39403fecbb",
+    "url": "/reactPortfolio/static/js/runtime-main.b25a74c4.js"
+  },
+  {
+    "revision": "631cc3b3063b50144759965db8f17d18",
+    "url": "/reactPortfolio/static/media/cmsGif.631cc3b3.gif"
+  },
+  {
+    "revision": "8eb258caf01f4c914b1fa7c6614ef68d",
+    "url": "/reactPortfolio/static/media/farmer.8eb258ca.gif"
+  },
+  {
+    "revision": "4352761b5f5cf44f5d78838aab4af468",
+    "url": "/reactPortfolio/static/media/profilePic.4352761b.png"
+  },
+  {
+    "revision": "c76dfc38b7ed4672f7cdbe927a3c9f03",
+    "url": "/reactPortfolio/static/media/realtor.c76dfc38.gif"
+  },
+  {
+    "revision": "ec82ad1768cb18bab7c685330efadac3",
+    "url": "/reactPortfolio/static/media/yosemiteDark.ec82ad17.jpg"
+  }
+]);
